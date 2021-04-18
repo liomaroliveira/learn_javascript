9d1b573ec5c0e17a0d793cea6982fbbd12b54af8
Liomar Oliveira <liomar.antonio@hotmail.com>
2021-04-18 09:40:31 -0300

    Add files via upload

---
+++ ex008.js
@@ -0,0 +1,8 @@
+var vel = 2
+console.log(`A velocidade do seu carro é ${vel} Km/h.`)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+} else {
+    console.log(`Está dentro da velocidade permitida.`)
+}
+console.log('Dirija sempre usando cinto de segurança!')
